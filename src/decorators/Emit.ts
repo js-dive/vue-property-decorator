0937318d3a65795800947b5ef657c832c9e882eb
gogoend <gogoend@qq.com>
2022-06-05 17:34:01 +0800

    docs: 了解Emit装饰器的实现

--- src/decorators/Emit.ts
+++ src/decorators/Emit.ts
@@ -12,11 +12,14 @@ const hyphenate = (str: string) => str.replace(hyphenateRE, '-$1').toLowerCase()
 export function Emit(event?: string) {
   return function (_target: Vue, propertyKey: string, descriptor: any) {
     const key = hyphenate(propertyKey)
+    // 原始的、被装饰的函数
     const original = descriptor.value
     descriptor.value = function emitter(...args: any[]) {
       const emit = (returnValue: any) => {
+        // 事件名称要么是从外面传入的事件，要么是被转化为连字符命名后的key
         const emitName = event || key
 
+        // 如果原始函数返回值为空值
         if (returnValue === undefined) {
           if (args.length === 0) {
             this.$emit(emitName)
@@ -26,11 +29,14 @@ export function Emit(event?: string) {
             this.$emit(emitName, ...args)
           }
         } else {
+          // 如果原始函数返回值不为空值
+          // 就把该返回值作为this.$emit事件名后第一个参数进行调用
           args.unshift(returnValue)
           this.$emit(emitName, ...args)
         }
       }
 
+      // 执行一下被装饰的函数，得到一个返回值
       const returnValue: any = original.apply(this, args)
 
       if (isPromise(returnValue)) {
